a48f4e07ae8ed89736445fa4cf213b7b3130f70c
joong2043 <tbvjqlqlqlr@gmail.com>
2022-08-04 23:28:06 +0900

    스프링 세팅 및 메인페이지에 지도 전체 화면으로 출력

---
+++ src/main/java/com/lion/youranmok/YourAnmokApplication.java
@@ -0,0 +1,23 @@
+package com.lion.youranmok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+//@RestController
+public class YourAnmokApplication {
+
+//	@RequestMapping("/")
+//	public String hello() {
+//		return "hello 다정";
+//	}
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(YourAnmokApplication.class, args);
+	}
+
+}
